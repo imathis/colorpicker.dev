da53d1615fcc5671f60e1daa94b918f4917349af
Brandon Mathis <brandon@imathis.com>
2024-09-16 09:10:05 -0500

    Moved some elements to index.html for better webcrawling

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,9 @@ import { ColorProvider, Picker } from './color'
 
 function App() {
   return (
-    <div>
-      <div className="banner">
-        <div className="preamble">A Most Excellent</div>
-        <h1>Color Picker</h1>
-        <div className="credit">Created for your enjoyment by <a rel="noreferrer" href="https://twitter.com/imathis" target="_blank">Brandon&nbsp;Mathis</a></div>
-      </div>
-      <ColorProvider>
-        <Picker />
-      </ColorProvider>
-    </div>
+    <ColorProvider>
+      <Picker />
+    </ColorProvider>
   )
 }
 
